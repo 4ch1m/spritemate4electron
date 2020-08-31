47a0db592fb610f5e7a2c7ae21e6a300c3eb302a
Achim <git@achimonline.de>
2020-08-31 17:44:36 +0200

    update to Electron 10; updated libraries; removal of "electron-window-state"

--- main.js
+++ main.js
@@ -1,20 +1,63 @@
 const {app, BrowserWindow, nativeImage} = require('electron');
-const windowStateKeeper = require('electron-window-state');
+const store = new (require('electron-store'))();
 
 let mainWindow;
 
-function createWindow() {
-    let mainWindowState = windowStateKeeper({
-        defaultWidth: 1024,
-        defaultHeight: 768
+function windowStateKeeper(windowName) {
+    let window;
+
+    // default values
+    let windowState = {
+        x: undefined,
+        y: undefined,
+        width: 1024,
+        height: 768
+    };
+
+    function setBounds() {
+        if (store.has(`windowState.${windowName}`)) {
+            windowState = store.get(`windowState.${windowName}`);
+        }
+    }
+
+    function saveState() {
+        if (!windowState.isMaximized) {
+            windowState = window.getBounds();
+        }
+        windowState.isMaximized = window.isMaximized();
+
+        store.set(`windowState.${windowName}`, windowState);
+    }
+
+    function track(win) {
+        window = win;
+
+        ['resize', 'move', 'close'].forEach(event => {
+            win.on(event, saveState);
+        });
+    }
+
+    setBounds();
+
+    return ({
+        x: windowState.x,
+        y: windowState.y,
+        width: windowState.width,
+        height: windowState.height,
+        isMaximized: windowState.isMaximized,
+        track
     });
+}
+
+function createWindow() {
+    const mainWindowStateKeeper = windowStateKeeper('main');
 
     mainWindow = new BrowserWindow({
         icon: nativeImage.createFromPath(__dirname + '/spritemate/dist/img/favicon/android-chrome-256x256.png'),
-        x: mainWindowState.x,
-        y: mainWindowState.y,
-        width: mainWindowState.width,
-        height: mainWindowState.height
+        x: mainWindowStateKeeper.x,
+        y: mainWindowStateKeeper.y,
+        width: mainWindowStateKeeper.width,
+        height: mainWindowStateKeeper.height
     });
     mainWindow.loadFile('spritemate/dist/index.html');
     mainWindow.setMenu(null);
@@ -22,7 +65,7 @@ function createWindow() {
         mainWindow = null
     });
 
-    mainWindowState.manage(mainWindow);
+    mainWindowStateKeeper.track(mainWindow);
 }
 
 app.on('ready', createWindow);
